f6fba552b0324ace90929eef2ffe42bef2e2c475
Nodir Bahodirov <123748282+bahodirov360@users.noreply.github.com>
2023-04-07 17:35:59 +0500

    Update api.py

--- ChatGPT/api.py
+++ ChatGPT/api.py
@@ -1,7 +1,7 @@
 import openai
 openai.api_key = "Your_API"
 
-prompt= input("Nima so'raysiz?: ")
+prompt= input("What are you looking for?: ")
 
 response = openai.Completion.create(engine="text-davinci-003",prompt=prompt
         ,max_tokens=1024)
